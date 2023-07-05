f645d512ed22d334e71c8bda6a931795481e0b3c
TwoNight <!Wooyoung0624>
2023-07-05 19:22:52 +0900

    상태 변형중

--- Assets/Script/MngBuild.cs
+++ Assets/Script/MngBuild.cs
@@ -5,18 +5,35 @@ using UnityEngine.UI;
 
 public class MngBuild : MonoBehaviour
 {
+
+    private enum BtnSelected
+    {
+        None,
+        Floor,
+        Wall,
+        Deco
+    }
+
+    
+
     //GameObject
     private GameObject Right; //UI가 있는 영역
     private GameObject BtnLayer;
+    
+    //Button
+    private Button FloorTab;
+    private Button WallTab;
+    private Button DecoTab;
+
+    //int
+    private int StateBtnNow;
+
 
     //bool
     private bool isFloorTab = false;
     private bool isWallTab = false;
     private bool isDecoTab = false;
 
-    private Button FloorTab;
-    private Button WallTab;
-    private Button DecoTab;
 
     private Color myBlue = new Color(0.5f, 0.5f, 1, 1);
 
@@ -39,6 +56,8 @@ public class MngBuild : MonoBehaviour
 
     private void initMng()
     {
+        StateBtnNow = (int)BtnSelected.None;
+        //Debug.Log(StateBtnNow);
         GameObject cansvas = GameObject.Find("Canvas");
         Right = cansvas.transform.Find("Right").gameObject;
 
@@ -53,21 +72,21 @@ public class MngBuild : MonoBehaviour
         FloorTab = btn.GetComponent<Button>();
         FloorTab.onClick.AddListener(() => 
         {
-            BtnSwitch(isFloorTab);
-            
+            BtnSwitch(ref isFloorTab);
+            BtnColorChange(FloorTab, isFloorTab);
         });
 
         btn = BtnLayer.transform.GetChild(1).gameObject;
         WallTab = btn.GetComponent<Button>();
         WallTab.onClick.AddListener(() => { 
-            BtnSwitch(isWallTab);
+            BtnSwitch(ref isWallTab);
             BtnColorChange(WallTab, isWallTab);
         });
 
         btn = BtnLayer.transform.GetChild(2).gameObject;
         DecoTab = btn.GetComponent<Button>();
         DecoTab.onClick.AddListener(() => { 
-            BtnSwitch(isDecoTab);
+            BtnSwitch(ref isDecoTab);
             BtnColorChange(DecoTab, isDecoTab);
         });
         btn = null;
@@ -98,17 +117,17 @@ public class MngBuild : MonoBehaviour
     /// 버튼 스위치
     /// </summary>
     /// <param name="_btn"> 바꿀 bool value</param>
-    public void BtnSwitch(bool _btnBool)
+    public void BtnSwitch(ref bool _btnBool)
     {
         _btnBool = !_btnBool;
         Debug.Log(_btnBool);
-        BtnColorChange(FloorTab, isFloorTab);
+       
     }
-    
+
     /// <summary>
     /// 버튼 색변경 함수
     /// </summary>
-    /// <param name="_btn"></param>
+    /// <param name="_btn">적용될 버튼</param>
     private void BtnColorChange(Button _btn, bool _btnbool)
     {
         if (_btnbool)//선택
