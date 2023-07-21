f368eb71b2e9cf2e06046ba37021f7d77e86a818
TwoNight <!Wooyoung0624>
2023-07-21 16:37:57 +0900

    MaxLog에 따라 메시지 개수를 조절하는  기능 추가

--- Assets/Script/MngLog.cs
+++ Assets/Script/MngLog.cs
@@ -10,7 +10,8 @@ public class MngLog : MonoBehaviour
     public GameObject logParent;
     public TMP_FontAsset NotoSansFont;
 
-    public int MaxLog;
+    private int MaxLog;
+    private int countLog;
 
     private void Awake()
     {
@@ -26,11 +27,23 @@ public class MngLog : MonoBehaviour
 
     private void Start()
     {
-        test();
-        test();
-        Deletelog();
+        countLog = 0;
+        MaxLog = 20;
+
+
+        //삭제 테스트 코드
+        //for(int i=0; i<25; i++)
+        //{
+        //    test();
+        //}
+        //Debug.Log(countLog);
+ 
     }
 
+    /// <summary>
+    /// content를 부모로 메시지를 추가함 
+    /// </summary>
+    /// <param name="_systemMessage"></param>
     public void addLog(string _systemMessage)
     {
         GameObject log = new GameObject();
@@ -45,6 +58,16 @@ public class MngLog : MonoBehaviour
 
         
         logText.text = _systemMessage;
+        countLog++;
+
+        if(countLog > 10)
+        {
+            for(int i=0; i<10; i++)
+            {
+                Deletelog(i);
+            }
+            countLog = 0;
+        }
     }
 
     public void test()
@@ -59,21 +82,39 @@ public class MngLog : MonoBehaviour
         logText.font = NotoSansFont;
 
         logText.text = "한글되나?";
+        countLog++;
 
-        
-        
+        if (countLog > MaxLog)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Deletelog(i);
+            }
+        }
     }
 
+    /// <summary>
+    /// 메시지 한개 없애기
+    /// </summary>
     public void Deletelog()
     {
         if(logParent.transform.childCount > 0)
         {
             Destroy(logParent.transform.GetChild(0).gameObject);
         }
-
-
     }
-    
+
+    /// <summary>
+    /// 오버로딩 인덱스를 없애기
+    /// </summary>
+    /// <param name="index"></param>
+    public void Deletelog(int index)
+    {
+        if (logParent.transform.childCount > 0)
+        {
+            Destroy(logParent.transform.GetChild(index).gameObject);
+        }
+    }
 
 
 }
